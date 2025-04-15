9124d45de4383332f9f4b58985864985a985719f
dimakruzha@gmail.com <dimakruzha@gmail.com>
2025-04-15 16:30:36 +0300

    Initial commit for DarkSouls-Sever.

---
+++ DarkSouls-Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using Test_WebSocket;
+
+namespace DarkSouls_Server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.shared.Start();
+
+            Console.WriteLine("Press Enter to send message.");
+            Console.ReadLine();
+            Server.shared.Send("SomeMessage");
+
+            Console.ReadKey();
+            Server.shared.Close();
+        }
+    }
+}
